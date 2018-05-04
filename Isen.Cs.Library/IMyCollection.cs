19003ce0dd79f57b167296e6860191c77aad1177
Thoril <artru.thomas.monchal@gmail.com>
2018-05-04 17:25:55 +0200

    Exercice sur les Classes avec Interface avec une Collection de String

---
+++ Isen.Cs.Library/IMyCollection.cs
@@ -0,0 +1,14 @@
+﻿using System.Dynamic;
+using System.Runtime.CompilerServices;
+
+namespace Isen.Cs.Library
+{
+    public interface IMyCollection
+    {
+        void Add(string item);
+        int Count { get; }
+        void InsertAt(int index, string item);
+        void RemoveAt(int index);
+        string[] Values { get; }
+    }
+}
